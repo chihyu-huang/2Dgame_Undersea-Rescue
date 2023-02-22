2d7af08757dac076e467e10b815af24bbbeac770
Joy Huang <106109437+BOBOeternal@users.noreply.github.com>
2023-02-23 04:38:54 +0800

    Add files via upload
    animation speed
    theme done(player/enemies/bullet)

--- src/MainWindow.java
+++ src/MainWindow.java
@@ -1,6 +1,4 @@
-import java.awt.BorderLayout;
-import java.awt.Color;
-import java.awt.Image;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyListener;
@@ -17,51 +15,32 @@ import javax.swing.JPanel;
 
 import util.UnitTests;
 
-/*
- * Created by Abraham Campbell on 15/01/2020.
- *   Copyright (c) 2020  Abraham Campbell
-
-Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the "Software"), to deal
-in the Software without restriction, including without limitation the rights
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:
-
-The above copyright notice and this permission notice shall be included in all
-copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-SOFTWARE.
-   
-   (MIT LICENSE ) e.g do what you want with this :-) 
- */ 
 
 
 
-public class MainWindow {
-	 private static  JFrame frame = new JFrame("Game");   // Change to the name of your game 
-	 private static   Model gameworld= new Model();
-	 private static   Viewer canvas = new  Viewer( gameworld);
-	 private KeyListener Controller =new Controller()  ; 
-	 private static   int TargetFPS = 100;
-	 private static boolean startGame= false; 
-	 private   JLabel BackgroundImageForStartMenu ;
+public class MainWindow extends JPanel{ //implements Runnable {
+
+	// title of the game
+	private static JFrame frame = new JFrame("Submarineee");
+	private static Model gameWorld = new Model();
+	private static Viewer canvas = new Viewer(gameWorld);
+	private KeyListener Controller =new Controller()  ;
+	private static int TargetFPS = 100;
+	private static boolean startGame= false;
+	private JLabel BackgroundImageForStartMenu;
 	  
 	public MainWindow() {
-		frame.setSize(1164, 764);  // you can customise this later and adapt it to change on size.
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);   //If exit // you can modify with your way of quitting , just is a template.
+		frame.setSize(1164, 764);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		//If exit // you can modify with your way of quitting , just is a template.
 		frame.setLayout(null);
 		frame.add(canvas);
+
 		canvas.setBounds(0, 0, 1164, 764);
-		canvas.setBackground(new Color(57,126,213)); //white background  replaced by Space background but if you remove the background method this will draw a white screen
+		canvas.setBackground(new Color(57,126,213));
 		canvas.setVisible(false);   // this will become visible after you press the key.
 
+//		startGameThread();
 
 		JButton startMenuButton = new JButton("Start Game");  // start button
 		startMenuButton.addActionListener(new ActionListener() {
@@ -75,14 +54,16 @@ public class MainWindow {
 				startGame = true;
 			}
 		});
-		startMenuButton.setBounds(400, 500, 200, 40);
+		startMenuButton.setBounds(0, 0, 200, 40);
 	        
 		//loading background image
-		File BackroundToLoad = new File("res/startscreen.png");  //should work okay on OSX and Linux but check if you have issues depending your eclipse install or if your running this without an IDE
+		File BackroundToLoad = new File("res/startscreen.png");
+//		File BackroundToLoad = new File("res/Background.png");
+		//should work okay on OSX and Linux but check if you have issues depending your eclipse install or if your running this without an IDE
 		try {
 			BufferedImage myPicture = ImageIO.read(BackroundToLoad);
 			BackgroundImageForStartMenu = new JLabel(new ImageIcon(myPicture));
-			BackgroundImageForStartMenu.setBounds(0, 0, 1000, 1000);
+			BackgroundImageForStartMenu.setBounds(0, 0, 1164, 764);
 			frame.add(BackgroundImageForStartMenu);
 		}  catch (IOException e) {
 			e.printStackTrace();
@@ -94,7 +75,8 @@ public class MainWindow {
 
 	public static void main(String[] args) {
 		MainWindow hello = new MainWindow();  //sets up environment 
-		while(true){   //not nice but remember we do just want to keep looping till the end.
+		while(true){
+			//not nice but remember we do just want to keep looping till the end.
 			// this could be replaced by a thread, but again we want to keep things simple
 
 			//swing has timer class to help us time this, but I'm writing my own,
@@ -111,8 +93,9 @@ public class MainWindow {
 			 if(startGame){
 				 gameloop();
 			 }
+
 			 //UNIT test to see if frame rate matches
-			UnitTests.CheckFrameRate(System.currentTimeMillis(),FrameCheck, TargetFPS);
+			UnitTests.CheckFrameRate(System.currentTimeMillis(), FrameCheck, TargetFPS);
 			  
 		}
 	} 
@@ -124,65 +107,73 @@ public class MainWindow {
 		// So no need to call it explicitly 
 		
 		// model update   
-		gameworld.gamelogic();
+		gameWorld.gamelogic();
 		// view update 
 		
 		canvas.updateview();
 		
 		//Both these calls could be setup as a thread, but we want to simplify the game logic for you.
 		//score update  
-		frame.setTitle("Score =  " + gameworld.getScore());
+		frame.setTitle("Score =  " + gameWorld.getScore());
 
 	}
 
+
+//	Thread gameThread;
+//
+//	public void startGameThread() {
+//		gameThread = new Thread(this);
+//		gameThread.start();
+//	}
+
+//	@Override
+//	public void run() {
+//
+//		double drawInterval = 1000 / TargetFPS;
+//		double nextDrawTime = System.currentTimeMillis() + drawInterval;
+//
+//
+//		while(gameThread != null){
+////			1 update info
+////			2 draw with the updated info
+//			update();
+//			repaint();
+//
+//			try{
+//				double remainingTime = nextDrawTime - System.currentTimeMillis();
+//
+//				if(remainingTime < 0){
+//					remainingTime = 0;
+//				}
+//
+//				Thread.sleep((long) remainingTime);
+//
+//				nextDrawTime += drawInterval;
+//
+//			}catch (InterruptedException e){
+//				e.printStackTrace();
+//			}
+//
+//
+//
+//		}
+//	}
+
+//	public void update(){
+//
+//	}
+
+//	public void paintComponent(Graphics g){
+//		super.paintComponent(g);
+//
+//		Graphics2D g2 = (Graphics2D) g;
+//
+//		g2.setColor(Color.white);
+//
+//		g2.fillRect(100, 100, 32, 32);
+//
+//		g2.dispose();
+//
+//	}
 }
 
-/*
- * 
- * 
-
-Hand shake agreement 
-::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
-:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::,=+++
-::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::,,,,,,:::::,=+++????
-::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::,,,,,,,,,,,,,,:++++????+??
-:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::,:,:,,:,:,,,,,,,,,,,,,,,,,,,,++++++?+++++????
-::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,=++?+++++++++++??????
-:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::,:,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,~+++?+++?++?++++++++++?????
-:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::,:::,,,,,,,,,,,,,,,,,,,,,,,,,,,~+++++++++++++++????+++++++???????
-::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::,:,,,,,,,,,,,,,,,,,,,,,,:===+=++++++++++++++++++++?+++????????????????
-:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::,,,,,,,,,,,,,,,,,,~=~~~======++++++++++++++++++++++++++????????????????
-::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::,::::,,,,,,=~.,,,,,,,+===~~~~~~====++++++++++++++++++++++++++++???????????????
-:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::,:,,,,,~~.~??++~.,~~~~~======~=======++++++++++++++++++++++++++????????????????II
-:::::::::::::::::::::::::::::::::::::::::::::::::::::::,:,,,,:=+++??=====~~~~~~====================+++++++++++++++++++++?????????????????III
-:::::::::::::::::::::::::::::::::::::::::::::::::::,:,,,++~~~=+=~~~~~~==~~~::::~~==+++++++==++++++++++++++++++++++++++?????????????????IIIII
-::::::::::::::::::::::::::::::::::::::::::::::::,:,,,:++++==+??+=======~~~~=~::~~===++=+??++++++++++++++++++++++++?????????????????I?IIIIIII
-::::::::::::::::::::::::::::::::::::::::::::::::,,:+????+==??+++++?++====~~~~~:~~~++??+=+++++++++?++++++++++??+???????????????I?IIIIIIII7I77
-::::::::::::::::::::::::::::::::::::::::::::,,,,+???????++?+?+++???7?++======~~+=====??+???++++++??+?+++???????????????????IIIIIIIIIIIIIII77
-:::::::::::::::::::::::::::::::::::::::,,,,,,=??????IIII7???+?+II$Z77??+++?+=+++++=~==?++?+?++?????????????III?II?IIIIIIIIIIIIIIIIIIIIIIIIII
-::::::::::::::::::::::::::::::,,,,,,~=======++++???III7$???+++++Z77ZDZI?????I?777I+~~+=7+?II??????????????IIIIIIIIIIIIIIIIIIIIII??=:,,,,,,,,
-::::::::,:,:,,,,,,,:::~==+=++++++++++++=+=+++++++???I7$7I?+~~~I$I??++??I78DDDO$7?++==~I+7I7IIIIIIIIIIIIIIIIII777I?=:,,,,,,,,,,,,,,,,,,,,,,,,
-++=++=++++++++++++++?+????+??????????+===+++++????I7$$ZZ$I+=~$7I???++++++===~~==7??++==7II?~,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,
-+++++++++++++?+++?++????????????IIIII?I+??I???????I7$ZOOZ7+=~7II?+++?II?I?+++=+=~~~7?++:,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,
-+?+++++????????????????I?I??I??IIIIIIII???II7II??I77$ZO8ZZ?~~7I?+==++?O7II??+??+=====.,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,
-?????????????III?II?????I?????IIIII???????II777IIII7$ZOO7?+~+7I?+=~~+???7NNN7II?+=+=++,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,
-????????????IIIIIIIIII?IIIIIIIIIIII????II?III7I7777$ZZOO7++=$77I???==+++????7ZDN87I??=~,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,
-IIII?II??IIIIIIIIIIIIIIIIIIIIIIIIIII???+??II7777II7$$OZZI?+$$$$77IIII?????????++=+.,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,
-IIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIII?+++?IIIII7777$$$$$$7$$$$7IIII7I$IIIIII???I+=,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,
-IIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIII???????IIIIII77I7777$7$$$II????I??I7Z87IIII?=,,,,,,,,,,,:,,::,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,
-777777777777777777777I7I777777777~,,,,,,,+77IIIIIIIIIII7II7$$$Z$?I????III???II?,,,,,,,,,,::,::::::::,,:,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,
-777777777777$77777777777+::::::::::::::,,,,,,,=7IIIII78ZI?II78$7++D7?7O777II??:,,,:,,,::::::::::::::,:,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,
-$$$$$$$$$$$$$77=:,:::::::::::::::::::::::::::,,7II$,,8ZZI++$8ZZ?+=ZI==IIII,+7:,,,,:::::::::::::::::,:::,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,
-$$$I~::::::::::::::::::::::::::::::::::::::::::II+,,,OOO7?$DOZII$I$I7=77?,,,,,,:::::::::::::::::::::,,,:,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,
-::::::::::::::::::::::::::::::::::::::::::::::::::::::+ZZ?,$ZZ$77ZZ$?,,,,,::::::::::::::::::::::::::,::::,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,
-:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::I$:::::::::::::::::::::::::::::::::::::::::::,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,
-:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::,,,:,,,,,,,,,,,,,,,,,,,,,,,,,,,,,
-:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::,,,,,,,,,,,,,,,,,,,,,,,,,,,
-:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::,,,,,,,,,,,,,,,,,,,,,,,,,,,
-:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::,,,,,,,,,,,,,,,,,,,,,,,,,
-::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::,,,,,,,,,,,,,,,,,,,,,,
-::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::,,,,,,,,,,,,,,,,,,,,,,
-                                                                                                                             GlassGiant.com
- * 
- * 
- */
--- src/Viewer.java
+++ src/Viewer.java
@@ -17,32 +17,7 @@ import javax.swing.Timer;
 import util.GameObject;
 
 
-/*
- * Created by Abraham Campbell on 15/01/2020.
- *   Copyright (c) 2020  Abraham Campbell
 
-Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the "Software"), to deal
-in the Software without restriction, including without limitation the rights
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:
-
-The above copyright notice and this permission notice shall be included in all
-copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NON-INFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-SOFTWARE.
-   
-   (MIT LICENSE ) e.g. do what you want with this :-)
- 
- * Credits: Kelly Charles (2020)
- */ 
 public class Viewer extends JPanel {
 	private long CurrentAnimationTime = 0;
 	
@@ -74,11 +49,13 @@ public class Viewer extends JPanel {
 	}
 	
 	
-	public void paintComponent(Graphics g) {
+	public void paintComponent(Graphics g2) {
 		
-		super.paintComponent(g);
-		CurrentAnimationTime++; // runs animation time step 
-		
+		super.paintComponent(g2);
+		Graphics2D g = (Graphics2D) g2;
+
+		CurrentAnimationTime++; // runs animation time step
+//		System.out.println(CurrentAnimationTime);
 		
 		//Draw player Game Object 
 		int x = (int) gameworld.getPlayer().getCentre().getX();
@@ -96,7 +73,8 @@ public class Viewer extends JPanel {
 		//Draw Bullets 
 		// change back 
 		gameworld.getBullets().forEach((temp) -> {
-			drawBullet((int) temp.getCentre().getX(), (int) temp.getCentre().getY(), (int) temp.getWidth(), (int) temp.getHeight(), temp.getTexture(),g);	 
+			drawBullet((int) temp.getCentre().getX(), (int) temp.getCentre().getY(),
+					temp.getWidth(), temp.getHeight(), temp.getTexture(), g);
 		}); 
 		
 		//Draw Enemies   
@@ -104,44 +82,32 @@ public class Viewer extends JPanel {
 			drawEnemies((int) temp.getCentre().getX(), (int) temp.getCentre().getY(), (int) temp.getWidth(), (int) temp.getHeight(), temp.getTexture(), g);
 	    }); 
 	}
-	
-	private void drawEnemies(int x, int y, int width, int height, String texture, Graphics g) {
-		File TextureToLoad = new File(texture);  //should work okay on OSX and Linux but check if you have issues depending your eclipse install or if your running this without an IDE 
-		try {
-			Image myImage = ImageIO.read(TextureToLoad);
-			//The spirte is 32x32 pixel wide and 4 of them are placed together so we need to grab a different one each time 
-			//remember your training :-) computer science everything starts at 0 so 32 pixels gets us to 31  
-			int currentPositionInAnimation = ((int) (CurrentAnimationTime % 4) * 32); //slows down animation so every 10 frames we get another frame so every 100ms
-			g.drawImage(myImage, x, y, x+width, y+height, currentPositionInAnimation,
-						0, currentPositionInAnimation+31, 32, null);
-			
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} 
-		
-	}
 
-	private void drawBackground(Graphics g) {
-//		File TextureToLoad = new File("res/spacebackground.png");  //should work okay on OSX and Linux but check if you have issues depending your eclipse install or if your running this without an IDE
-		File TextureToLoad = new File("res/Large/Background.png");  //should work okay on OSX and Linux but check if you have issues depending your eclipse install or if your running this without an IDE
+	private void drawEnemies(int x, int y, int width, int height, String texture, Graphics g) {
+		File TextureToLoad = new File(texture);
 		try {
 			Image myImage = ImageIO.read(TextureToLoad);
-			g.drawImage(myImage, 0,0, 1164, 764,
-						0 , 0, 1164, 764, null);
+			//The sprite is 32x32 pixel wide and 4 of them are placed together, so we need to grab a different one each time
+			//remember your training :-) computer science everything starts at 0 so 32 pixels gets us to 31
+			int currentPositionInAnimation = ((int) (CurrentAnimationTime % 12) / 3  * 22);
+			//slows down animation so every 10 frames we get another frame so every 100ms
+			g.drawImage(myImage, x, y, x+width, y+height,
+					currentPositionInAnimation, 0, currentPositionInAnimation+21, 22, null);
+
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 	}
-	
+
 	private void drawBullet(int x, int y, int width, int height, String texture,Graphics g) {
 		File TextureToLoad = new File(texture);  //should work okay on OSX and Linux but check if you have issues depending your eclipse install or if your running this without an IDE 
 		try {
 			Image myImage = ImageIO.read(TextureToLoad); 
-			//64 by 128 
+			//64 by 128
+			int currentPositionInAnimation= ((int) ((CurrentAnimationTime % 20) / 5 )) * 18;
 			 g.drawImage(myImage, x,y, x+width, y+height,
-					 	0 , 0, 63, 127, null);
+					 	0 , 0, currentPositionInAnimation + 17, 13, null);
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -155,9 +121,9 @@ public class Viewer extends JPanel {
 			Image myImage = ImageIO.read(TextureToLoad);
 			//The sprite is 32x32 pixel wide and 4 of them are placed together, so we need to grab a different one each time
 			//remember your training :-) computer science everything starts at 0 so 32 pixels gets us to 31  
-			int currentPositionInAnimation= ((int) ((CurrentAnimationTime % 40) / 10)) * 32; //slows down animation so every 10 frames we get another frame so every 100ms
+			int currentPositionInAnimation= ((int) ((CurrentAnimationTime % 12) / 3)) * 20; //slows down animation so every 10 frames we get another frame so every 100ms
 			g.drawImage(myImage, x, y, x+width, y+height, currentPositionInAnimation ,
-						0, currentPositionInAnimation + 31, 32, null);
+						0, currentPositionInAnimation + 19, 16, null);
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -167,56 +133,18 @@ public class Viewer extends JPanel {
 		// Bullets from https://opengameart.org/forumtopic/tatermands-art 
 		// background image from https://www.needpix.com/photo/download/677346/space-stars-nebula-background-galaxy-universe-free-pictures-free-photos-free-images
 	}
+
+	private void drawBackground(Graphics g) {
+//		File TextureToLoad = new File("res/spacebackground.png");  //should work okay on OSX and Linux but check if you have issues depending your eclipse install or if your running this without an IDE
+		File TextureToLoad = new File("res/Background.png");  //should work okay on OSX and Linux but check if you have issues depending your eclipse install or if your running this without an IDE
+		try {
+			Image myImage = ImageIO.read(TextureToLoad);
+			g.drawImage(myImage, 0,0, 1164, 764,
+					0 , 0, 1164, 764, null);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
 }
 
-
-/*
- * 
- * 
- *              VIEWER HMD into the world                                                             
-                                                                                
-                                      .                                         
-                                         .                                      
-                                             .  ..                              
-                               .........~++++.. .  .                            
-                 .   . ....,++??+++?+??+++?++?7ZZ7..   .                        
-         .   . . .+?+???++++???D7I????Z8Z8N8MD7I?=+O$..                         
-      .. ........ZOZZ$7ZZNZZDNODDOMMMMND8$$77I??I?+?+=O .     .                 
-      .. ...7$OZZ?788DDNDDDDD8ZZ7$$$7I7III7??I?????+++=+~.                      
-       ...8OZII?III7II77777I$I7II???7I??+?I?I?+?+IDNN8??++=...                  
-     ....OOIIIII????II?I??II?I????I?????=?+Z88O77ZZO8888OO?++,......            
-      ..OZI7III??II??I??I?7ODM8NN8O8OZO8DDDDDDDDD8DDDDDDDDNNNOZ= ......   ..    
-     ..OZI?II7I?????+????+IIO8O8DDDDD8DNMMNNNNNDDNNDDDNDDNNNNNNDD$,.........    
-      ,ZII77II?III??????DO8DDD8DNNNNNDDMDDDDDNNDDDNNNDNNNNDNNNNDDNDD+.......   .
-      7Z??II7??II??I??IOMDDNMNNNNNDDDDDMDDDDNDDNNNNNDNNNNDNNDMNNNNNDDD,......   
- .  ..IZ??IIIII777?I?8NNNNNNNNNDDDDDDDDNDDDDDNNMMMDNDMMNNDNNDMNNNNNNDDDD.....   
-      .$???I7IIIIIIINNNNNNNNNNNDDNDDDDDD8DDDDNM888888888DNNNNNNDNNNNNNDDO.....  
-       $+??IIII?II?NNNNNMMMMMDN8DNNNDDDDZDDNN?D88I==INNDDDNNDNMNNMNNNNND8:..... 
-   ....$+??III??I+NNNNNMMM88D88D88888DDDZDDMND88==+=NNNNMDDNNNNNNMMNNNNND8......
-.......8=+????III8NNNNMMMDD8I=~+ONN8D8NDODNMN8DNDNNNNNNNM8DNNNNNNMNNNNDDD8..... 
-. ......O=??IIIIIMNNNMMMDDD?+=?ONNNN888NMDDM88MNNNNNNNNNMDDNNNMNNNMMNDNND8......
-........,+++???IINNNNNMMDDMDNMNDNMNNM8ONMDDM88NNNNNN+==ND8NNNDMNMNNNNNDDD8......
-......,,,:++??I?ONNNNNMDDDMNNNNNNNNMM88NMDDNN88MNDN==~MD8DNNNNNMNMNNNDND8O......
-....,,,,:::+??IIONNNNNNNDDMNNNNNO+?MN88DN8DDD888DNMMM888DNDNNNNMMMNNDDDD8,.... .
-...,,,,::::~+?+?NNNNNNNMD8DNNN++++MNO8D88NNMODD8O88888DDDDDDNNMMMNNNDDD8........
-..,,,,:::~~~=+??MNNNNNNNND88MNMMMD888NNNNNNNMODDDDDDDDND8DDDNNNNNNDDD8,.........
-..,,,,:::~~~=++?NMNNNNNNND8888888O8DNNNNNNMMMNDDDDDDNMMNDDDOO+~~::,,,.......... 
-..,,,:::~~~~==+?NNNDDNDNDDNDDDDDDDDNNND88OOZZ$8DDMNDZNZDZ7I?++~::,,,............
-..,,,::::~~~~==7DDNNDDD8DDDDDDDD8DD888OOOZZ$$$7777OOZZZ$7I?++=~~:,,,.........   
-..,,,,::::~~~~=+8NNNNNDDDMMMNNNNNDOOOOZZZ$$$77777777777II?++==~::,,,......  . ..
-...,,,,::::~~~~=I8DNNN8DDNZOM$ZDOOZZZZ$$$7777IIIIIIIII???++==~~::,,........  .  
-....,,,,:::::~~~~+=++?I$$ZZOZZZZZ$$$$$777IIII?????????+++==~~:::,,,...... ..    
-.....,,,,:::::~~~~~==+?II777$$$$77777IIII????+++++++=====~~~:::,,,........      
-......,,,,,:::::~~~~==++??IIIIIIIII?????++++=======~~~~~~:::,,,,,,.......       
-.......,,,,,,,::::~~~~==+++???????+++++=====~~~~~~::::::::,,,,,..........       
-.........,,,,,,,,::::~~~======+======~~~~~~:::::::::,,,,,,,,............        
-  .........,.,,,,,,,,::::~~~~~~~~~~:::::::::,,,,,,,,,,,...............          
-   ..........,..,,,,,,,,,,::::::::::,,,,,,,,,.,....................             
-     .................,,,,,,,,,,,,,,,,.......................                   
-       .................................................                        
-           ....................................                                 
-               ....................   .                                         
-                                                                                
-                                                                                
-                                                                 GlassGiant.com
-                                                                 */
